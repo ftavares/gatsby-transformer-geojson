c1d9e0501e47a034946b5adfc83f5a4f9757e5f5
Fabio Tavares <fabio.tavares@capita.co.uk>
2019-07-15 11:55:46 +0100

    packages update

---
+++ gatsby-node.js
@@ -0,0 +1,50 @@
+const _ = require(`lodash`);
+
+const fs = require('fs');
+
+async function onCreateNode({
+  node,
+  actions,
+  createNodeId,
+  createContentDigest
+}) {
+  const {
+    createNode,
+    createParentChildLink
+  } = actions;
+
+  function transformObject(obj, id, type, relativePath) {
+    const jsonNode = { ...obj,
+      id,
+      children: [],
+      parent: node.id,
+      internal: {
+        contentDigest: createContentDigest(obj),
+        type,
+        relativePath
+      }
+    };
+    createNode(jsonNode);
+    createParentChildLink({
+      parent: node,
+      child: jsonNode
+    });
+  }
+
+  if (node.internal.mediaType !== `application/geo+json`) {
+    return;
+  }
+
+  const content = await fs.readFileSync(node.absolutePath);
+  const parsedContent = JSON.parse(content);
+
+  if (_.isArray(parsedContent)) {
+    parsedContent.forEach((obj, i) => {
+      transformObject(obj, obj.id ? obj.id : createNodeId(`${node.id} [${i}] >>> GEOJSON`), 'GeoJson', node.relativePath);
+    });
+  } else if (_.isPlainObject(parsedContent)) {
+    transformObject(parsedContent, parsedContent.id ? parsedContent.id : createNodeId(`${node.id} >>> GEOJSON`), 'GeoJson', node.relativePath);
+  }
+}
+
+exports.onCreateNode = onCreateNode;
